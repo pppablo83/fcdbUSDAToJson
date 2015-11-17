2fa76395bc425c21c7fc4075d944de8ba824d8d0
Pablo Pérez <pppablo83@gmail.com>
2015-11-17 16:07:26 +0200

    Parsing of the food description file completed

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
 var fs = require('fs')
-var jsonfile = require('jsonfile')
 var mkdirp = require('mkdirp')
 var argv = require('minimist')(process.argv.slice(2));
 var parser = require('./parser/parser')
-
-//Data version
-var NUTRIENT_DATABASE_VERSION='28'
-var NUTRIENT_DATABASE_DOWNLOAD_URL='http://www.ars.usda.gov/Services/docs.htm?docid=25700'
-
-//Delimiters for the files distributed http://ndb.nal.usda.gov/
-var FIELD_DELIMITER='^'
-var STRING_DELIMITER='~'
 
 //Output dir, can be overriden by command line parameters
 var DEFAULT_OUTPUT_DIR='./output'
 var outputDir = DEFAULT_OUTPUT_DIR
 
 console.log('Current script works with data from, USDA, url= ' +
-  NUTRIENT_DATABASE_DOWNLOAD_URL + ', version=' + NUTRIENT_DATABASE_VERSION)
+  parser.url + ', version=' + parser.version)
 
 if(argv.help) {
   console.log("Usage: node main.js\n" +
@@ -33,7 +24,14 @@ mkdirp.sync(outputDir)
 
 console.log('Files to be created in: ' + outputDir)
 
-//TODO start the work
+//Sync process
+try {
+  parser.foodDescription('./sr28asc/FOOD_DES.txt', outputDir)
+} catch(e) {
+  console.error("An error producing the output files has happened: " + e)
+  process.exit(1)
+}
+
 
 //TODO find appropiate mention and move to README.md
 console.log('Special thanks to USDA for all the effort in producing this valuable set of data!')
--- parser/parser.js
+++ parser/parser.js
@@ -1,6 +1,50 @@
-module.exports = {
-  foodDescription: function() {
+var jsonfile = require('jsonfile')
+//Delimiters for the files distributed http://ndb.nal.usda.gov/
+var FIELD_DELIMITER='^'
+var REGEX_STRING_DELIMITER=/~/g
 
+//Data version
+var NUTRIENT_DATABASE_VERSION='28'
+var NUTRIENT_DATABASE_DOWNLOAD_URL='http://www.ars.usda.gov/Services/docs.htm?docid=25700'
+
+module.exports = {
+  foodDescription: function(fileToRead, outputDir) {
+    console.log('Fields in food description module: NDB_No, FdGrp_Cd,' +
+      'Long_Desc, Shrt_Desc, ComName, ManufacName, Survey, Ref_desc, Refuse,' +
+      'SciName, N_Factor, Pro_Factor, Fat_Factor, CHO_Factor')
+    var rl = require('readline').createInterface({
+      input: require('fs').createReadStream(fileToRead),
+      terminal: false
+    });
+
+    rl.on('line', function (line) {
+      var fields = line.replace(REGEX_STRING_DELIMITER, '').split(FIELD_DELIMITER)
+      var obj = {
+        ndb_no: fields[0],
+        foodGroup: fields[1],
+        nomenclature_english: {
+          short: fields[2],
+          long: fields[3],
+          otherCommonNames: fields[4]
+        },
+        manufacturerName: fields[5],
+        survey: fields[6],
+        ref_desc: fields[7],
+        refuse: fields[8],
+        scientific_name: fields[9],
+        nitrogen_factor: fields[10],
+        protein_factor: fields[11],
+        fat_factor: fields[12],
+        cho_factor: fields[13]
+      }
+
+      var fileName = fields[0] + '.json';
+      jsonfile.writeFile(outputDir + '/' + fileName, obj, function(err) {
+        if(err) {
+          throw(err)
+        }
+      })
+    });
   },
   nutrientData: function() {
 
@@ -34,5 +78,7 @@ module.exports = {
   },
   sourcesOfData: function() {
 
-  }
+  },
+  version: NUTRIENT_DATABASE_VERSION,
+  url: NUTRIENT_DATABASE_DOWNLOAD_URL
 }
