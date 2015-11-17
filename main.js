1cfb24fec737fc65d67c760892a2976488b2d686
pablo <pppablo83@gmail.com>
2015-11-17 18:15:58 +0200

    Added async to better control the call flow

--- main.js
+++ main.js
@@ -1,37 +1,48 @@
-var fs = require('fs')
 var mkdirp = require('mkdirp')
+var async = require('async')
 var argv = require('minimist')(process.argv.slice(2));
 var parser = require('./parser/parser')
 
-//Output dir, can be overriden by command line parameters
-var DEFAULT_OUTPUT_DIR='./output'
-var outputDir = DEFAULT_OUTPUT_DIR
+//Output dir, can be overridden by command line parameters
+var DEFAULT_OUTPUT_DIR = './output'
 
 console.log('Current script works with data from, USDA, url= ' +
-  parser.url + ', version=' + parser.version)
+    parser.url + ', version=' + parser.version)
 
-if(argv.help) {
-  console.log("Usage: node main.js\n" +
-    "\t--outputDir=Directory\tPuts the output files in the chosen directory," +
-    "otherwise default is: " + DEFAULT_OUTPUT_DIR +"\n" +
-    "\t--help\tShows help on use and exits")
-  process.exit(0);
+if (argv.help) {
+    console.log("Usage: node main.js\n" +
+        "\t--outputDir=Directory\tPuts the output files in the chosen directory," +
+        "otherwise default is: " + DEFAULT_OUTPUT_DIR + "\n" +
+        "\t--help\tShows help on use and exits")
+    process.exit(0);
 }
 
 var outputDir = argv.outputDir ? argv.outputDir : DEFAULT_OUTPUT_DIR;
-
 mkdirp.sync(outputDir)
-
 console.log('Files to be created in: ' + outputDir)
 
-//Sync process
-try {
-  parser.foodDescription('./sr28asc/FOOD_DES.txt', outputDir)
-} catch(e) {
-  console.error("An error producing the output files has happened: " + e)
-  process.exit(1)
-}
+async.series([
+    function (callback) {
+        try {
+            parser.foodDescription('./sr28asc/FOOD_DES.txt', outputDir, callback)
+        } catch (e) {
+            callback(e, null)
+        }
+    },
+    function (callback) {
+        //TODO find appropiate mention and move to README.md
+        console.log('Special thanks to USDA for all the effort in producing this valuable set of data!')
+        callback(null, null)
+    }
+], function (err, results) {
+    if (err) {
+        console.error('An error producing the output files has happened: ' + e)
+        process.exit(1)
+    } else {
+        console.log('Finished process successfully')
+        process.exit(0)
+    }
+})
 
 
-//TODO find appropiate mention and move to README.md
-console.log('Special thanks to USDA for all the effort in producing this valuable set of data!')
+
--- parser/parser.js
+++ parser/parser.js
@@ -1,84 +1,93 @@
 var jsonfile = require('jsonfile')
 //Delimiters for the files distributed http://ndb.nal.usda.gov/
-var FIELD_DELIMITER='^'
-var REGEX_STRING_DELIMITER=/~/g
+var FIELD_DELIMITER = '^'
+var REGEX_STRING_DELIMITER = /~/g
 
 //Data version
-var NUTRIENT_DATABASE_VERSION='28'
-var NUTRIENT_DATABASE_DOWNLOAD_URL='http://www.ars.usda.gov/Services/docs.htm?docid=25700'
+var NUTRIENT_DATABASE_VERSION = '28'
+var NUTRIENT_DATABASE_DOWNLOAD_URL = 'http://www.ars.usda.gov/Services/docs.htm?docid=25700'
 
 module.exports = {
-  foodDescription: function(fileToRead, outputDir) {
-    console.log('Fields in food description module: NDB_No, FdGrp_Cd,' +
-      'Long_Desc, Shrt_Desc, ComName, ManufacName, Survey, Ref_desc, Refuse,' +
-      'SciName, N_Factor, Pro_Factor, Fat_Factor, CHO_Factor')
-    var rl = require('readline').createInterface({
-      input: require('fs').createReadStream(fileToRead),
-      terminal: false
-    });
+    foodDescription: function (fileToRead, outputDir, callback) {
+        console.log('Fields in food description module: NDB_No, FdGrp_Cd,' +
+            'Long_Desc, Shrt_Desc, ComName, ManufacName, Survey, Ref_desc, Refuse,' +
+            'SciName, N_Factor, Pro_Factor, Fat_Factor, CHO_Factor')
+        var rl = require('readline').createInterface({
+            input: require('fs').createReadStream(fileToRead),
+            terminal: false
+        });
 
-    rl.on('line', function (line) {
-      var fields = line.replace(REGEX_STRING_DELIMITER, '').split(FIELD_DELIMITER)
-      var obj = {
-        ndb_no: fields[0],
-        foodGroup: fields[1],
-        nomenclature_english: {
-          short: fields[2],
-          long: fields[3],
-          otherCommonNames: fields[4]
-        },
-        manufacturerName: fields[5],
-        survey: fields[6],
-        ref_desc: fields[7],
-        refuse: fields[8],
-        scientific_name: fields[9],
-        nitrogen_factor: fields[10],
-        protein_factor: fields[11],
-        fat_factor: fields[12],
-        cho_factor: fields[13]
-      }
+        var callbacks = 0;
 
-      var fileName = fields[0] + '.json';
-      jsonfile.writeFile(outputDir + '/' + fileName, obj, function(err) {
-        if(err) {
-          throw(err)
-        }
-      })
-    });
-  },
-  nutrientData: function() {
+        rl.on('line', function (line) {
+            callbacks++
+            var fields = line.replace(REGEX_STRING_DELIMITER, '').split(FIELD_DELIMITER)
+            var obj = {
+                ndb_no: fields[0],
+                foodGroup: fields[1],
+                nomenclature_english: {
+                    short: fields[2],
+                    long: fields[3],
+                    otherCommonNames: fields[4]
+                },
+                manufacturerName: fields[5],
+                survey: fields[6],
+                ref_desc: fields[7],
+                refuse: fields[8],
+                scientific_name: fields[9],
+                nitrogen_factor: fields[10],
+                protein_factor: fields[11],
+                fat_factor: fields[12],
+                cho_factor: fields[13]
+            }
 
-  },
-  weight: function() {
+            var fileName = fields[0] + '.json';
+            jsonfile.writeFile(outputDir + '/' + fileName, obj, function (err) {
+                console.log(fileName)
+                if (err) {
+                    throw err
+                } else {
+                    callbacks--
+                }
+                if(callbacks < 1) {
+                    return callback()
+                }
+            })
+        });
+    },
+    nutrientData: function () {
 
-  },
-  footNote: function() {
+    },
+    weight: function () {
 
-  },
-  foodGroupDescription: function() {
+    },
+    footNote: function () {
 
-  },
-  langualFactor: function() {
+    },
+    foodGroupDescription: function () {
 
-  },
-  langualFactorsDescription: function() {
+    },
+    langualFactor: function () {
 
-  },
-  nutrientDefinition: function() {
+    },
+    langualFactorsDescription: function () {
 
-  },
-  sourceCode: function() {
+    },
+    nutrientDefinition: function () {
 
-  },
-  dataDerivationCodeDescription: function() {
+    },
+    sourceCode: function () {
 
-  },
-  sourcesOfDataLink: function() {
+    },
+    dataDerivationCodeDescription: function () {
 
-  },
-  sourcesOfData: function() {
+    },
+    sourcesOfDataLink: function () {
 
-  },
-  version: NUTRIENT_DATABASE_VERSION,
-  url: NUTRIENT_DATABASE_DOWNLOAD_URL
+    },
+    sourcesOfData: function () {
+
+    },
+    version: NUTRIENT_DATABASE_VERSION,
+    url: NUTRIENT_DATABASE_DOWNLOAD_URL
 }
