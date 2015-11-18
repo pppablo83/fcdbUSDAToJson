7708a1366602a1eab6885993e188e6674bf5cf1e
pablo <pppablo83@gmail.com>
2015-11-18 11:58:20 +0200

    Finished parsing of the abbreviated file.

--- main.js
+++ main.js
@@ -14,46 +14,73 @@ if (argv.help) {
         "\t--outputDir=Directory\tPuts the output files in the chosen directory," +
         "otherwise default is: " + DEFAULT_OUTPUT_DIR + "\n" +
         "\t--help\tShows help on use and exits" + "\n" +
-        "\t--langualInfo\tAdds langual data to the result")
+        "\t--langualInfo\tAdds langual data to the result" + "\n" +
+        "\t--abbrv\tParsing only the abbrev version, in case it is called, files will be produced in an " +
+        "abbrv subfolder of the output dir")
     process.exit(0);
 }
 
 var outputDir = argv.outputDir ? argv.outputDir : DEFAULT_OUTPUT_DIR;
+outputDir += argv.abbrv ? '/abbrv' : ''
 mkdirp.sync(outputDir)
 console.log('Files to be created in: ' + outputDir)
 
 var step = 0
-async.series([
-    function (callback) {
-        step++
-        parser.foodDescription('./sr28asc/FOOD_DES.txt', outputDir, step, callback)
-    },
-    function (callback) {
-        step++
-        parser.foodGroupDescription('./sr28asc/FD_GROUP.txt', outputDir, step, callback)
-    },
-    function (callback) {
-        if (argv.langualInfo) {
+
+if (argv.abbrv) {
+    async.series([
+        function (callback) {
             step++
-            parser.langualFactor('./sr28asc/LANGUAL.txt', outputDir, step, callback)
-        } else {
-            return callback()
+            parser.abbrv('./sr28abbr/ABBREV.txt', outputDir, step, callback)
+        },
+        function (callback) {
+            //TODO find appropiate mention and move to README.md
+            console.log('Special thanks to USDA for all the effort in producing this valuable set of data!')
+            callback(null, null)
         }
-    },
-    function (callback) {
-        //TODO find appropiate mention and move to README.md
-        console.log('Special thanks to USDA for all the effort in producing this valuable set of data!')
-        callback(null, null)
-    }
-], function (err, results) {
-    if (err) {
-        console.error('An error producing the output files has happened: ' + err)
-        process.exit(1)
-    } else {
-        console.log('Finished process successfully')
-        process.exit(0)
-    }
-})
+    ], function (err, results) {
+        if (err) {
+            console.error('An error producing the output files has happened: ' + err)
+            process.exit(1)
+        } else {
+            console.log('Finished process successfully')
+            process.exit(0)
+        }
+    })
+} else {
+    async.series([
+        function (callback) {
+            step++
+            parser.foodDescription('./sr28asc/FOOD_DES.txt', outputDir, step, callback)
+        },
+        function (callback) {
+            step++
+            parser.foodGroupDescription('./sr28asc/FD_GROUP.txt', outputDir, step, callback)
+        },
+        function (callback) {
+            if (argv.langualInfo) {
+                step++
+                parser.langualFactor('./sr28asc/LANGUAL.txt', outputDir, step, callback)
+            } else {
+                return callback()
+            }
+        },
+        function (callback) {
+            //TODO find appropiate mention and move to README.md
+            console.log('Special thanks to USDA for all the effort in producing this valuable set of data!')
+            callback(null, null)
+        }
+    ], function (err, results) {
+        if (err) {
+            console.error('An error producing the output files has happened: ' + err)
+            process.exit(1)
+        } else {
+            console.log('Finished process successfully')
+            process.exit(0)
+        }
+    })
+}
+
 
 
 
--- parser/parser.js
+++ parser/parser.js
@@ -11,6 +11,16 @@ var REGEX_STRING_DELIMITER = /~/g
 var NUTRIENT_DATABASE_VERSION = '28'
 var NUTRIENT_DATABASE_DOWNLOAD_URL = 'http://www.ars.usda.gov/Services/docs.htm?docid=25700'
 
+//Units literals
+var units = {
+    gramsPer100g: 'g/100g',
+    milligramsPer100g: 'mg/100g',
+    microgramsPer100g: 'µg/100g',
+    IU_Per100g: 'IU/100g',
+    Kcal_Per100g: 'kcal/100g'
+};
+
+
 module.exports = {
     foodDescription: function (fileToRead, outputDir, step, callback) {
         console.log('Starting Step ' + step + '...')
@@ -23,28 +33,28 @@ module.exports = {
             terminal: false
         });
 
-        var callbacks = 0;
+        var callbacks = 0
 
         rl.on('line', function (line) {
             callbacks++
             var fields = line.replace(REGEX_STRING_DELIMITER, '').split(FIELD_DELIMITER)
             var obj = {
-                ndb_no: fields[0],
-                foodGroup_english: fields[1],
-                nomenclature_english: {
+                ndbNo: fields[0],
+                foodGroupEnglish: fields[1],
+                nomenclatureEnglish: {
                     short: fields[2],
                     long: fields[3],
                     otherCommonNames: fields[4]
                 },
                 manufacturerName: fields[5],
                 survey: fields[6],
-                ref_desc: fields[7],
+                refDesc: fields[7],
                 refuse: fields[8],
-                scientific_name: fields[9],
-                nitrogen_factor: fields[10],
-                protein_factor: fields[11],
-                fat_factor: fields[12],
-                cho_factor: fields[13]
+                scientificName: fields[9],
+                nitrogenFactor: fields[10],
+                proteinFactor: fields[11],
+                fatFactor: fields[12],
+                choFactor: fields[13]
             }
 
             var fileName = fields[0] + '.json';
@@ -98,7 +108,7 @@ module.exports = {
                             if (err) {
                                 callbackInternal(err)
                             } else {
-                                obj.foodGroup_english = foodDescriptionObject[obj.foodGroup_english.toString()]
+                                obj.foodGroupEnglish = foodDescriptionObject[obj.foodGroupEnglish.toString()]
                                 jsonFile.writeFileSync(directoryToRead + '/' + file, obj)
                                 callbacks--
                             }
@@ -171,6 +181,95 @@ module.exports = {
     },
     sourcesOfData: function () {
 
+    },
+    abbrv: function (fileToRead, outputDir, step, callback) {
+        console.log('Starting Step ' + step + '...')
+        console.log('Fields in food description module: NDB_No, Shrt_Desc, Water, Energ_Kcal, ' +
+            'Protein, Lipid_Tot, Ash, Carbohydrt, Fiber_TD, Sugar_Tot, Calcium, Iron, Magnesium,' +
+            'Phosphorus, Potassium, Sodium, Zinc, Copper, Manganese, Selenium, Vit_C, Thiamin,' +
+            'Riboflavin, Niacin, Panto_acid, Vit_B6, Folate_Tot, Folic_acid, Food_Folate, ' +
+            'Folate_DFE, Choline_Tot, Vit_B12, Vit_A_IU, Vit_A_RAE, Retinol, Alpha_Carot,' +
+            'Beta_Carot, Beta_Crypt, Lycopene, Lut+ZEA, Vit_E, Vit_D_mcg, VIT_D_IU, VIT_K,' +
+            'FA_Sat, FA_Mono, FA_Poly, Cholestrl, GmWt_1, GmWt_Desc1, GmWt_2, GmWt_Desc2' +
+            'Refuse_Pct')
+
+        var rl = require('readline').createInterface({
+            input: require('fs').createReadStream(fileToRead),
+            terminal: false
+        });
+
+        var callbacks = 0
+
+        rl.on('line', function (line) {
+            callbacks++
+            var fields = line.replace(REGEX_STRING_DELIMITER, '').split(FIELD_DELIMITER)
+            var obj = {
+                ndbNo: fields[0],
+                shrtDesc: fields[1],
+                water: {value: fields[2], unit: units.gramsPer100g},
+                energKcal: {value: fields[3], unit: units.Kcal_Per100g},
+                protein: {value: fields[4], unit: units.gramsPer100g},
+                lipidTot: {value: fields[5], unit: units.gramsPer100g},
+                ash: {value: fields[6], unit: units.gramsPer100g},
+                carbohydrt: {value: fields[7], unit: units.gramsPer100g},
+                fiberTD: {value: fields[8], unit: units.gramsPer100g},
+                sugarTot: {value: fields[9], unit: units.gramsPer100g},
+                calcium: {value: fields[10], unit: units.milligramsPer100g},
+                iron: {value: fields[11], unit: units.milligramsPer100g},
+                magnesium: {value: fields[12], unit: units.milligramsPer100g},
+                phosphorus: {value: fields[13], unit: units.milligramsPer100g},
+                potassium: {value: fields[14], unit: units.milligramsPer100g},
+                sodium: {value: fields[15], unit: units.milligramsPer100g},
+                zinc: {value: fields[16], unit: units.milligramsPer100g},
+                copper: {value: fields[17], unit: units.milligramsPer100g},
+                manganese: {value: fields[18], unit: units.milligramsPer100g},
+                selenium: {value: fields[19], unit: units.microgramsPer100g},
+                vitC: {value: fields[20], unit: units.milligramsPer100g},
+                thiamin: {value: fields[21], unit: units.milligramsPer100g},
+                riboflavin: {value: fields[22], unit: units.milligramsPer100g},
+                niacin: {value: fields[23], unit: units.milligramsPer100g},
+                pantoAcid: {value: fields[24], unit: units.milligramsPer100g},
+                vitB6: {value: fields[25], unit: units.milligramsPer100g},
+                folateTot: {value: fields[26], unit: units.microgramsPer100g},
+                folicAcid: {value: fields[27], unit: units.microgramsPer100g},
+                foodFolate: {value: fields[28], unit: units.microgramsPer100g},
+                folateDFE: {value: fields[29], unit: units.microgramsPer100g},
+                cholineTot: {value: fields[30], unit: units.milligramsPer100g},
+                vitB12: {value: fields[31], unit: units.microgramsPer100g},
+                vitA_IU: {value: fields[32], unit: units.IU_Per100g},
+                vitA_RAE: {value: fields[33], unit: units.microgramsPer100g},
+                retinol: {value: fields[34], unit: units.microgramsPer100g},
+                alphaCarot: {value: fields[35], unit: units.microgramsPer100g},
+                betaCarot: {value: fields[36], unit: units.microgramsPer100g},
+                betaCrypt: {value: fields[37], unit: units.microgramsPer100g},
+                lycopene: {value: fields[38], unit: units.microgramsPer100g},
+                lutZEA: {value: fields[39], unit: units.microgramsPer100g},
+                vitE: {value: fields[40], unit: units.milligramsPer100g},
+                vitD_mcg: {value: fields[41], unit: units.microgramsPer100g},
+                vitD_IU: {value: fields[42], unit: units.IU_Per100g},
+                vitK: {value: fields[43], unit: units.microgramsPer100g},
+                faSat: {value: fields[44], unit: units.gramsPer100g},
+                faMono: {value: fields[45], unit: units.gramsPer100g},
+                faPoly: {value: fields[46], unit: units.gramsPer100g},
+                cholesttrl: {value: fields[47], unit: units.milligramsPer100g},
+                gmWt1: {value: fields[48], desc: fields[49]},
+                gmWt2: {value: fields[50], desc: fields[51]},
+                refusePct: fields[52]
+            }
+
+            var fileName = fields[0] + '.json';
+            jsonFile.writeFile(outputDir + '/' + fileName, obj, function (err) {
+                if (err) {
+                    callback(err)
+                } else {
+                    callbacks--
+                }
+                if (callbacks < 1) {
+                    console.log('Step ' + step + ' completed.')
+                    return callback()
+                }
+            })
+        });
     },
     version: NUTRIENT_DATABASE_VERSION,
     url: NUTRIENT_DATABASE_DOWNLOAD_URL
